f5051f2a7af1a1c2e01c6e9766456d29307c502c
Jen Garner <jgarner.py@gmail.com>
2020-12-05 15:47:08 -0500

    add function call to fab to log a test message; make fab focusable

--- app/src/main/java/com/jj/honeypot/PresentsActivity.java
+++ app/src/main/java/com/jj/honeypot/PresentsActivity.java
@@ -3,6 +3,8 @@ package com.jj.honeypot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class PresentsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class PresentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presents);
+    }
+
+    public void onClickPresentsFab(View view) {
+        Log.d("test", "hello from fab");
     }
 }
