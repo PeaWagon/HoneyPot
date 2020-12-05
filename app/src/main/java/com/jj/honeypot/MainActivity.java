4915c1e0c52638dcb7a5d0cb5c11ee4416280ab8
Jen Garner <jgarner.py@gmail.com>
2020-12-05 14:54:47 -0500

    add new activities and layouts for honey, presents, and reminders

--- app/src/main/java/com/jj/honeypot/MainActivity.java
+++ app/src/main/java/com/jj/honeypot/MainActivity.java
@@ -2,7 +2,9 @@ package com.jj.honeypot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickHoneyButton(View view) {
+        Intent honeyActivityIntent = new Intent(this, HoneyActivity.class);
+        startActivity(honeyActivityIntent);
+    }
+
+    public void onClickPresentsButton(View view) {
+        Intent presentsActivityIntent = new Intent(this, PresentsActivity.class);
+        startActivity(presentsActivityIntent);
+    }
+
+    public void onClickRemindersButton(View view) {
+        Intent remindersActivityIntent = new Intent(this, RemindersActivity.class);
+        startActivity(remindersActivityIntent);
     }
 }
---
+++ app/src/main/java/com/jj/honeypot/PresentsActivity.java
@@ -0,0 +1,14 @@
+package com.jj.honeypot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presents);
+    }
+}
